0fffe982b3fe3029811dc39458a37c0cf910e825
praveen453 <pjayakody524@gmail.com>
2024-03-03 20:00:08 +0530

    Added Question

--- inky.java
+++ inky.java
@@ -1,3 +1,14 @@
+/*Inside the class Inky, Create an object called Ponkey.
+Create a local variable inside the main method Pinkey and its value is "donkey".
+Print variable Pinky inside the constructor.
+Create an Other variable Inside the main method as blinky. It's value is "Pinkey".
+There is another variable inside the main method rinkey. It's value is "monkey".
+Pass all 3 variables to another constructor and Create below statements.
+1) Pinky donkey
+2) donkey, monkey Monkey
+3) pinky, donkey, Monkey */
+
+
 public class inky {
 
     public inky(String pinkey){
